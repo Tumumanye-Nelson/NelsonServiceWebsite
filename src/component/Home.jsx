1bbcf9ec6843edc1b9b69217681ae13049228c39
Tumumanye-Nelson <tumumanyenelson@gmail.com>
2024-12-30 15:51:27 +0300

    added some css

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,35 +1,34 @@
-import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-
-  const linkTab = () => {
-    navigate("/login"); // Assuming "/login" is the route for the Login component
-  };
+ 
+ 
 
   return (
-    <div className="Home">
+    <div className="Home" >
       <div>
         <h1 className="amin">
-          This is the website for Nelson<br /> services where new friends can<br /> be
-          created
+          Welcome to Nelson Services<br />
+          A place where new friendships<br />
+          are created
         </h1>
         <p className="ppp">
-          Connect and Expand Your Circle: Our platform helps you meet new
+          Connect and expand your circle! Our platform helps you meet new
           people, make meaningful connections, and build friendships. Whether
-         you are looking for casual chats, shared interests, or lifelong
-          bonds, we provide a safe and welcoming space for you<br /> to explore and
-          connect with others worldwide.
+          you are looking for casual chats, shared interests, or lifelong
+          bonds, we provide a safe and welcoming space for you<br />
+          to explore and connect with others worldwide.
         </p>
         <nav>
-          <button className="aaa" onClick={linkTab}>
+          <button className="aaa">
             Login Please
           </button>
         </nav>
       </div>
-      <img src="/images/home3.jpg" alt="" className="im" />
-    
-
+      <img
+        src="/images/home3.jpg"
+        alt="A welcoming space for making new friends"
+        className="im"
+      />
     </div>
   );
 }
