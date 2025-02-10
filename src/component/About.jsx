97654f4c4947807de90424aa961ae903a2ed214a
Tumumanye-Nelson <tumumanyenelson@gmail.com>
2025-02-10 19:18:10 +0300

    added content in pages

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,31 +1,31 @@
 function About() {
   return (
-    <div id="about" className="about">
-      <div>
-        <div className="about12">
-          <h1>About Page</h1>
-        </div>
+    <div id="about12" className="about12">
+      <h1>About Page</h1>
 
-     
-
-        {/* Move the images section below the paragraph */}
-        <div className="image-column">
-          <h1 className="rrry">What we do best</h1>
+      {/* Grid Container */}
+      <div className="about-grid">
+        {/* Image Section */}
+        <div className="image-section">
           <div className="image-gallery">
             <img src="src/photo/designer.jpg" alt="Designer" className="imgg" />
             <img src="src/photo/desin2.jpg" alt="Design 2" className="imgg" />
             <img src="src/photo/hello.jpg" alt="Hello" className="imgg" />
             <img src="src/photo/pex.jpg" alt="Pex" className="imgg" />
           </div>
+        </div>
+
+        {/* Text Section */}
+        <div className="text-section">
+
           <p className="py">
-          We deliver awesome designs and fully responsive static
-          <br />
-          websites for your business capable of being optimized for better search engine visibility
-          <br />
-          so that you can get highly ranked in all search engines. Our team also develops user-friendly,
-          <br />
-          jet-fast, fully mobile-optimized, responsive, and SEO-friendly websites.
-        </p>
+          <h2 className="section-title">What we do best</h2>
+
+            We deliver awesome designs and fully responsive static websites for your business,
+            capable of being optimized for better search engine visibility so that you can get
+            highly ranked in all search engines. Our team also develops user-friendly, 
+            jet-fast, fully mobile-optimized, responsive, and SEO-friendly websites.
+          </p>
         </div>
       </div>
     </div>
@@ -33,3 +33,4 @@ function About() {
 }
 
 export default About;
+
