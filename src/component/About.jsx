e31a274b956d5fc695b56428a57b54bedfdb8548
Tumumanye-Nelson <tumumanyenelson@gmail.com>
2025-02-26 20:09:58 +0300

    added  css

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div id="about12" className="about12">
-      <h1>About Page</h1>
+      <h1 className="oy">About Page</h1>
 
       {/* Grid Container */}
       <div className="about-grid">
@@ -20,7 +20,6 @@ function About() {
 
           <p className="py">
           <h2 className="section-title">What we do best</h2>
-
             We deliver awesome designs and fully responsive static websites for your business,
             capable of being optimized for better search engine visibility so that you can get
             highly ranked in all search engines. Our team also develops user-friendly, 
